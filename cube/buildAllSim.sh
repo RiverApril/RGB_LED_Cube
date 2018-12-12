0b96051aa15a643fb5086398cd9be3627a071bcf
nedearb <nedearb@gmail.com>
2018-12-11 22:04:49 -0700

    updated build for sim

--- cube/buildAllSim.sh
+++ cube/buildAllSim.sh
@@ -4,7 +4,9 @@
 ./buildGameSim.sh Snake &
 ./buildGameSim.sh Reversi &
 ./buildGameSim.sh Effects &
-./buildGameSim.sh 2PSnake &
-./buildGameSim.sh Fludd &
+#./buildGameSim.sh 2PSnake &
+#./buildGameSim.sh Fludd &
+./buildGameSim.sh PuzzleCube &
+./buildGameSim.sh Connect4 &
 
 g++ -std=c++11 cubeMenu/cubeMenu.cpp cubeCore/cubeCore.cpp cubeCore/geometry.cpp cubeCore/color.cpp cubeCore/perlin.cpp cubeCore/visualRenderer.cpp `pkg-config --cflags --libs sdl2` -o menu
